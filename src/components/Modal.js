8fa32a91366857df04197f34f67c087ceee321dd
oley-projects <oley.for.git@gmail.com>
2021-12-10 21:42:23 +0200

    players name length check

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,3 +1,5 @@
+// State
+import { useState } from "react";
 // Styles
 import styled from "styled-components";
 
@@ -10,28 +12,35 @@ const Modal = (
     setSecondPlayerName,
   }
   ) => {
-  const playerNamesSaveHandler = () => {
-    setHiddenModal(!hiddenModal);
-  }
-  const playerFirstHandler = (e) => {
-    firstPlayerName = e.target.value;
-    setFirstPlayerName(firstPlayerName);
-  }
-  const playerSecondHandler = (e) => {
-    secondPlayerName = e.target.value;
-    setSecondPlayerName(secondPlayerName);
-  }
-  return (
-    <StyledOverlayModal>
-      <StyledModal>
-        <label htmlFor="player1">Player 1 Name</label>
-        <input onChange={playerFirstHandler} value={firstPlayerName} type="text" id="player1" minLength="3" maxLength="20" />
-        <label htmlFor="player2">Player 2 Name</label>
-        <input onChange={playerSecondHandler} value={secondPlayerName} type="text" id="player2" minLength="3" maxLength="20" />
-        <button onClick={playerNamesSaveHandler}>Start</button>
-      </StyledModal>
-    </StyledOverlayModal>
-  );
+    const [characterMinCheck, setCharacterMinCheck] = useState(false);
+    const playerNamesSaveHandler = () => {
+      if (firstPlayerName.length >= 3 && secondPlayerName.length >= 3) {
+        setHiddenModal(!hiddenModal);
+      } else {
+        setCharacterMinCheck(true);
+      }
+      setTimeout(() => setCharacterMinCheck(false), 2000);
+    }
+    const playerFirstHandler = (e) => {
+      firstPlayerName = e.target.value;
+      setFirstPlayerName(firstPlayerName);
+    }
+    const playerSecondHandler = (e) => {
+      secondPlayerName = e.target.value;
+      setSecondPlayerName(secondPlayerName);
+    }
+    return (
+      <StyledOverlayModal>
+        <StyledModal>
+          <label htmlFor="player1">Player 1 Name</label>
+          <input onChange={playerFirstHandler} value={firstPlayerName} type="text" id="player1" minLength="3" maxLength="20" />
+          <label htmlFor="player2">Player 2 Name</label>
+          <input onChange={playerSecondHandler} value={secondPlayerName} type="text" id="player2" minLength="3" maxLength="20" />
+          <button onClick={playerNamesSaveHandler}>Start</button>
+          {characterMinCheck && <div>Player's name too short, minimum 3 characters</div>}
+        </StyledModal>
+      </StyledOverlayModal>
+    );
 };
 
 const StyledOverlayModal = styled.div`
@@ -49,6 +58,7 @@ const StyledModal = styled.div`
   background-color: rgb(44, 55, 85);
   padding: 3rem;
   border-radius: 1rem;
+  position: relative;
   label {
     color: #fff;
     text-align: center;
@@ -78,6 +88,15 @@ const StyledModal = styled.div`
       background-color: rgb(80, 90, 120);
       color: #ddd;
     }
+  }
+  div {
+    width: 70%;
+    color: red;
+    text-align: center;
+    position: absolute;
+    top: 3%;
+    left: 50%;
+    transform: translateX(-50%);
   }
 `;
 
